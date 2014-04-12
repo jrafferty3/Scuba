c8256f730dd4b72c6d8df35bb9b8bff4b6eceb66
jrafferty3 <jrafferty3@gatech.edu>
2014-04-12 11:43:35 -0400

    new swimming
    look at and pointer dont work yet but are disabled

---
+++ Assets/Resources/scripts/lookAt.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAt: MonoBehaviour
+{
+	//values that will be set in the Inspector
+	public Transform Target;
+	public float RotationSpeed;
+	
+	//values for internal use
+	private Quaternion _lookRotation;
+	private Vector3 _direction;
+	
+	// Update is called once per frame
+	void Update()
+	{
+		//find the vector pointing from our position to the target
+		transform.LookAt (Target);
+	}
+}
---
+++ Assets/Resources/scripts/swimming.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swimming : MonoBehaviour {
+	public int start_distance = 4;
+	private int distance;
+	private bool collision = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			distance = start_distance*2;
+		} else {
+			distance = start_distance;
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime*(collision?-1:1);
+		} else if (Input.GetKey (KeyCode.S)) {
+			transform.position = transform.position + -Camera.main.transform.forward * distance * Time.deltaTime*(collision?-1:1);
+		}
+		if (Input.GetKey (KeyCode.Q)) {
+			transform.position = transform.position + Camera.main.transform.up * distance * Time.deltaTime*(collision?-1:1);
+		} else if (Input.GetKey (KeyCode.E)) {
+			transform.position = transform.position + -Camera.main.transform.up * distance * Time.deltaTime*(collision?-1:1);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position = transform.position + -Camera.main.transform.right * distance * Time.deltaTime*(collision?-1:1);
+		}else if (Input.GetKey (KeyCode.D)) {
+			transform.position = transform.position + Camera.main.transform.right * distance * Time.deltaTime*(collision?-1:1);
+		}
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		collision = true;
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		collision = false;
+	}
+}
