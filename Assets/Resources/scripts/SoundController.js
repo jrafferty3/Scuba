ad0cce82de746ccbdfce498356e0329b01b5f258
nintendo424 <mattermaker@hotmail.com>
2014-04-13 12:07:46 -0400

    Sound Script beginning
    Started sound script

---
+++ Assets/Resources/scripts/SoundController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var lowOxygen: AudioClip;
+
+var keySounds =  [["o",lowOxygen]];
+function Start () {
+	
+}
+
+function Update () {
+	for (var i=0; i<keySounds.length; i++) 
+		print(i);
+}
