0d79b49b228b2febf6ccfd12e8071cf8d8b49ab4
jrafferty3 <jrafferty3@gatech.edu>
2014-04-16 19:24:24 -0400

    cant fly out of water, check for surface

--- Assets/Resources/scripts/swimming.cs
+++ Assets/Resources/scripts/swimming.cs
@@ -2,21 +2,27 @@
 using System.Collections;
 
 public class swimming : MonoBehaviour {
-	public int start_distance = 4;
-	private int distance;
+	private float start_distance = 2f;
+	private float max_dist = 8f;
+	public float distance;
 	private bool collision = false;
+	private bool fast = false;
+	public static bool surface = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		distance = start_distance;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		fast = false;
 		if (Input.GetKey (KeyCode.LeftShift)) {
-			distance = start_distance*2;
+			if(distance < max_dist)
+				distance+=0.1f;
 		} else {
-			distance = start_distance;
+			if(distance > start_distance)
+				distance-=0.1f;
 		}
 		if (Input.GetKey (KeyCode.W)) {
 			transform.position = transform.position + Camera.main.transform.forward * distance * Time.deltaTime*(collision?-1:1);
@@ -24,6 +30,9 @@ public class swimming : MonoBehaviour {
 			transform.position = transform.position + -Camera.main.transform.forward * distance * Time.deltaTime*(collision?-1:1);
 		}
 		if (Input.GetKey (KeyCode.Q)) {
+			if(distance > 4){
+				fast = true;
+			}
 			transform.position = transform.position + Camera.main.transform.up * distance * Time.deltaTime*(collision?-1:1);
 		} else if (Input.GetKey (KeyCode.E)) {
 			transform.position = transform.position + -Camera.main.transform.up * distance * Time.deltaTime*(collision?-1:1);
@@ -33,6 +42,23 @@ public class swimming : MonoBehaviour {
 		}else if (Input.GetKey (KeyCode.D)) {
 			transform.position = transform.position + Camera.main.transform.right * distance * Time.deltaTime*(collision?-1:1);
 		}
+		if (this.transform.position.y > 0) {
+			this.transform.position = new Vector3(this.transform.position.x,0,this.transform.position.z);
+		}
+		if (fast) {
+			//play alarm
+		}
+		if (this.transform.position.y > -0.5) {
+			setSurface(true);
+		} else {
+			setSurface(false);
+		}
+		if (surface)
+			Debug.Log ("surface");
+	}
+
+	static void setSurface(bool set){
+		surface = set;
 	}
 
 	void OnCollisionEnter (Collision col)
