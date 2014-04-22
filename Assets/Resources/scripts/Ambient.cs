05967c4918acbd04a827fc05d539b4a062a1f577
Chul <scahn.x@gmail.com>
2014-04-22 00:47:10 -0400

    Added Ambient Sounds
    Ambient sounds have been added and can be changed by pressing "h"

---
+++ Assets/Resources/scripts/Ambient.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ambient : MonoBehaviour {
+	public GameObject one, two, three;
+	public int j=0;
+	private bool keyDown = false;
+
+	// Use this for initialization
+	void Start () {
+		one = GameObject.Find ("Amb1");
+		two = GameObject.Find ("Amb2");
+		three = GameObject.Find ("Amb3");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.H)	) {
+			j++;
+			keyDown=true;
+		}
+
+		if (Input.GetKey("h")&& keyDown) {
+			//keyDown=false;
+		}
+
+		if (j % 3 == 0) {
+		
+			if (one.audio.isPlaying == false)
+			{
+				one.audio.Play();
+			}
+			two.audio.Pause ();
+			three.audio.Pause ();
+
+		}
+
+		if (j % 3 == 2) {
+
+			if (three.audio.isPlaying == false)
+			{
+				three.audio.Play();
+			}
+			two.audio.Pause ();
+			one.audio.Pause ();
+		}
+
+		if (j % 3 == 1) {
+
+			if (two.audio.isPlaying == false)
+			{
+				two.audio.Play();
+			}
+			one.audio.Pause ();
+			three.audio.Pause ();
+				
+		}
+	
+
+	}
+}
